80c189e99c90bb6c8341f4d86c059742d3b7000d
youneskm1002 <youneskm890@gmail.com>
2026-06-29 02:37:31 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TA_VRAIE_API_KEY",
+  apiKey: "AIzaSyD0FUpy_VX1I8j9AX285t4fvqDPvndNn3s",
   authDomain: "smart-teacher-toolkit.firebaseapp.com",
   projectId: "smart-teacher-toolkit",
-  storageBucket: "smart-teacher-toolkit.appspot.com",
-  messagingSenderId: "TON_VRAI_MESSAGING_SENDER_ID",
-  appId: "TON_VRAI_APP_ID"
+  storageBucket: "smart-teacher-toolkit.firebasestorage.app",
+  messagingSenderId: "191719425389",
+  appId: "1:191719425389:web:a7efec0a81c428480ac664",
+  measurementId: "G-7QRQEE8EML"
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
