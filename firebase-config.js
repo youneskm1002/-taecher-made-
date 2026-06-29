1d1ea6d0415d5d362ece0f1953648a59ed0f47f7
youneskm1002 <youneskm890@gmail.com>
2026-06-29 02:22:41 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "COLLE_ICI_TON_API_KEY",
+  apiKey: "TA_VRAIE_API_KEY",
   authDomain: "smart-teacher-toolkit.firebaseapp.com",
   projectId: "smart-teacher-toolkit",
   storageBucket: "smart-teacher-toolkit.appspot.com",
-  messagingSenderId: "COLLE_ICI_TON_MESSAGING_SENDER_ID",
-  appId: "COLLE_ICI_TON_APP_ID"
+  messagingSenderId: "TON_VRAI_MESSAGING_SENDER_ID",
+  appId: "TON_VRAI_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
